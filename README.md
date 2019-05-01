01b71b5591c1208c726ed6dd2a0090e77bcd97dd
Feng Du <fengdu_xiangyang@163.com>
2019-05-01 09:09:49 +0800

    init repository

---
+++ README.md
@@ -0,0 +1,8 @@
+# c case study
+> 最好的学习就是不断的实践
+
+![image](https://github.com/HP-dufeng/my-reading-list/blob/master/images/practice.png)
+
+
+
+- [pointers on c](pointers-on-c) -- the book of 'pointers on c'
---
+++ pointers-on-c/rearrange/cmake-build-debug/CMakeFiles/rearrange.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rearrange.dir/main.c.o"
+  "rearrange.pdb"
+  "rearrange"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/rearrange.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
