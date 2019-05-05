2101093a403db06ae3f21bb49100905e04f23118
Feng Du <fengdu_xiangyang@163.com>
2019-05-05 14:14:22 +0800

    add queue

---
+++ pointers-on-c/queue/queue.h
@@ -0,0 +1,22 @@
+/*
+** interface of queue
+*/
+
+#include <stdlib.h>
+
+#define QUEUE_TYPE int
+
+void create_queue(size_t size);
+
+void destroy_queue(void);
+
+void insert(QUEUE_TYPE value);
+
+void remove(void);
+
+QUEUE_TYPE first();
+
+int is_empty(void);
+
+int is_full(void);
+
