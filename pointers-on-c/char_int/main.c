ee1fd2e4fa672e23f2cf678ce7faa029fd25e630
Feng Du <fengdu_xiangyang@163.com>
2019-05-05 12:18:48 +0800

    add stack implemented

---
+++ pointers-on-c/char_int/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int read_int() {
+    int value;
+    int ch;
+
+    value = 0;
+
+    while((ch = getchar()) != EOF && isdigit(ch)) {
+        value *= 10;
+        value += ch - '0';
+    }
+
+    ungetc(ch, stdin);
+
+    return value;
+
+}
+
+int main() {
+    int value;
+
+    value = read_int();
+
+    printf("%d\n", value);
+}
---
+++ pointers-on-c/stack/stack.h
@@ -0,0 +1,18 @@
+#include <stddef.h>
+
+// The type of stack
+#define STACK_TYPE int 
+
+void push(STACK_TYPE value);
+
+void pop(void);
+
+STACK_TYPE top(void);
+
+int is_empty(void);
+
+int is_full(void);
+
+
+void create_stack(size_t size);
+void destroy_stack(void);
